222226ca7c918b2776bb5004582041b6c6055a46
Sparkie <teeraphat.su@ku.th>
2020-02-08 22:50:03 +0700

    Linked list in lecture class

---
+++ LectureArray.c
@@ -0,0 +1,24 @@
+// //ARRAY//
+// int a[10]; // index = a[0] to a[9],count = 10
+// index start from 0 !!
+// if declare value in array != array size. Other space will be 0
+// Ex. A[10] = {1,2,3}    =  {1,2,3,0,0,0,0,0,0,0}
+// Print every value in array by using index and loop
+// for(int i = 0; i < ____; i++)
+//    printf("%d",A[i]);
+// Array size should be constant. by using #define or const type
+// const and define number can't be changed
+// define effect all program. const effect in scope.
+
+//// PASS ARRAY TO ANOTHER FUNCTION ////
+// always pass array and size //
+// Ex. int addOne(int A[], int size); //
+
+// Pass by value send value to function //
+// Pass by reference send address to funcyion //
+
+// 2D ARRAY //
+// int A[x][y].   x is row and y is column
+// A[0][0] A[0][1] A[0][2]
+// A[1][0] A[1][1] A[1][2]
+// A[2][0] A[2][1] A[2][2]
